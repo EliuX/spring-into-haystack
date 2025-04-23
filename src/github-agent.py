9e5be7df19cdf55aeb3b379ab369f5bd91034c7e
EliuX <eliecerhdz@gmail.com>
2025-04-23 13:56:44 -0400

    feat: add Pipeline to fetch README

--- src/github-agent.py
+++ src/github-agent.py
@@ -1,40 +1,117 @@
+import os
+import shutil
+from typing import List
+from dotenv import load_dotenv
+from haystack import Pipeline, component
 from haystack.components.generators.chat import OpenAIChatGenerator
-from haystack.dataclasses import ChatMessage
-from haystack.components.agents import Agent
+from haystack.dataclasses import ChatMessage 
 from haystack_integrations.tools.mcp import MCPTool, StdioServerInfo
+from haystack.components.tools import ToolInvoker   
+from haystack.components.converters import OutputAdapter
+        
+@component
+class MessageRouter:
+    @component.output_types(nextAction=List[ChatMessage], forAnalysis=List[ChatMessage], response=List[ChatMessage])
+    def run(self, 
+            from_user: List[ChatMessage] = None,
+            tool_messages: List[ChatMessage] = None,
+            messages: List[ChatMessage] = None):
+        msg = messages or from_user or tool_messages 
+        if msg[0].tool_calls:
+            print("- Tool call detected: ", msg[0])
+            return {"nextAction": msg}
+        elif from_user or tool_messages or (messages and msg[0].from_tool):
+            print("- Content to be analyzed: ", msg[0])
+            return {"forAnalysis": msg}          
+        else:
+            print("- Final content: ", msg[0])
+            return {"response": msg}   
 
+load_dotenv()
+
+if not shutil.which("docker"):
+    raise RuntimeError("Required docker command not found")
+
+github_token = os.getenv('GITHUB_PERSONAL_ACCESS_TOKEN')
+if not github_token:
+    raise ValueError("GITHUB_PERSONAL_ACCESS_TOKEN environment variable not set.")
+
+github_repo_name = os.getenv('GITHUB_REPO_NAME')
+if not github_repo_name:
+    raise ValueError("GITHUB_REPO_NAME environment variable not set.")
 
 github_mcp_server = StdioServerInfo(
-        ## TODO: Add correct params for the Github MCP Server (official or legacy)
-        env={
-            "GITHUB_PERSONAL_ACCESS_TOKEN": "<YOUR GITHUB TOKEN>" # DO NOT ADD IT TO YOUR COMMIT
-        }
-    )
+    command="npx",
+    args=[
+        "-y",
+        "@modelcontextprotocol/server-github"
+    ],
+    env={
+        "GITHUB_PERSONAL_ACCESS_TOKEN": github_token, 
+        "GITHUB_TOOLSETS": "all"
+    },
+)
 
 print("MCP server is created")
 
-## TODO: Create your tools here:
-tool_1 = MCPTool(...
+get_github_file_contents_tool = MCPTool(name="get_file_contents", server_info=github_mcp_server, description="Get contents of a file or directory from Github")
+create_github_issue_tool = MCPTool(name="create_issue", server_info=github_mcp_server, description="Create a new issue in a GitHub repository")
 
-tools = [## TODO: Add tools tool_1, tool_2, tool_3,..]
+tools = [get_github_file_contents_tool, create_github_issue_tool]
 
 print("MCP tools are created")
 
-## TODO: Create your Agent here:
-agent = Agent(
-...
-)
+# There is a bug in the MCPTool use of asyncio that prevents it from being used as a tool in the agent.
+# agent = Agent(
+#     chat_generator=OpenAIChatGenerator(),
+#     system_prompt="""
+#     You are a helpful Agent that can read Github repositories content 
+#     and create issues.
+#     """,
+#     tools=tools,
+#     exit_conditions=["create_issue"],
+# )
+
+agent = Pipeline()
+agent.add_component("llm", OpenAIChatGenerator(tools=tools))
+agent.add_component("tool_invoker", ToolInvoker(tools=tools)) 
+agent.add_component("router", MessageRouter())
+# agent.add_component(
+#     "adapter",
+#     OutputAdapter(
+#         template="{{ tool_messages }}",
+#         output_type=list[ChatMessage],
+#         unsafe=True,
+#     ),
+# )
+
+
+agent.connect("router.forAnalysis", "llm.messages")
+agent.connect("llm.replies", "router.messages")
+agent.connect("router.nextAction", "tool_invoker.messages")
+agent.connect("tool_invoker.tool_messages", "router.tool_messages")
+
+agent.draw("logo/agent.png") # Check on the design
+
 
 print("Agent created")
 
-## Example query to test your agent
-user_input = "Can you find the typo in the README of <owner/repo> and open an issue about how to fix it?"
+## Query to test agent
+user_input = f"""
+Fetch for the content of the README.md of the Github repository {github_repo_name}, if not available.
+If the content is available look for typos. If typos are found then create an issue in the repository.
+After the issue is created, return the number of typos found (0 if none).
+"""
 
-## (OPTIONAL) Feel free to add other example queries that can be resolved with this Agent
-
-response = agent.run(messages=[ChatMessage.from_user(text=user_input)])
+response = agent.run({
+    "router": {
+        "from_user": [ChatMessage.from_user(user_input)]
+    }
+})
 
 ## Print the agent thinking process
 print(response)
-## Print the final response
-print(response["messages"][-1].text)
+
+# Print the final response
+# for msg in response["response"]:
+#     print(msg.text)
