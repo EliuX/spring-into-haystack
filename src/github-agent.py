386675232d2a6f23c31cf1a5f3e87742f87b82e3
EliuX <eliecerhdz@gmail.com>
2025-04-23 20:27:04 -0400

    feat: add functional Agent
    Ignore deepcopy of MCPTool

--- src/github-agent.py
+++ src/github-agent.py
@@ -1,31 +1,19 @@
 import os
+import pprint
 import shutil
+from asyncio import Future
 from typing import List
 from dotenv import load_dotenv
-from haystack import Pipeline, component
+from haystack import component
 from haystack.components.generators.chat import OpenAIChatGenerator
 from haystack.dataclasses import ChatMessage 
 from haystack_integrations.tools.mcp import MCPTool, StdioServerInfo
-from haystack.components.tools import ToolInvoker   
-from haystack.components.converters import OutputAdapter
-        
-@component
-class MessageRouter:
-    @component.output_types(nextAction=List[ChatMessage], forAnalysis=List[ChatMessage], response=List[ChatMessage])
-    def run(self, 
-            from_user: List[ChatMessage] = None,
-            tool_messages: List[ChatMessage] = None,
-            messages: List[ChatMessage] = None):
-        msg = messages or from_user or tool_messages 
-        if msg[0].tool_calls:
-            print("- Tool call detected: ", msg[0])
-            return {"nextAction": msg}
-        elif from_user or tool_messages or (messages and msg[0].from_tool):
-            print("- Content to be analyzed: ", msg[0])
-            return {"forAnalysis": msg}          
-        else:
-            print("- Final content: ", msg[0])
-            return {"response": msg}   
+from haystack.components.agents import Agent
+
+ 
+class SafeMCPTool(MCPTool):
+    def __deepcopy__(self, memo):
+        return memo # Do no copy, reuse the instance
 
 load_dotenv()
 
@@ -54,44 +42,23 @@ github_mcp_server = StdioServerInfo(
 
 print("MCP server is created")
 
-get_github_file_contents_tool = MCPTool(name="get_file_contents", server_info=github_mcp_server, description="Get contents of a file or directory from Github")
-create_github_issue_tool = MCPTool(name="create_issue", server_info=github_mcp_server, description="Create a new issue in a GitHub repository")
+get_github_file_contents_tool = SafeMCPTool(name="get_file_contents", server_info=github_mcp_server, description="Get contents of a file or directory from Github")
+create_github_issue_tool = SafeMCPTool(name="create_issue", server_info=github_mcp_server, description="Create a new issue in a GitHub repository")
 
 tools = [get_github_file_contents_tool, create_github_issue_tool]
 
 print("MCP tools are created")
 
 # There is a bug in the MCPTool use of asyncio that prevents it from being used as a tool in the agent.
-# agent = Agent(
-#     chat_generator=OpenAIChatGenerator(),
-#     system_prompt="""
-#     You are a helpful Agent that can read Github repositories content 
-#     and create issues.
-#     """,
-#     tools=tools,
-#     exit_conditions=["create_issue"],
-# )
-
-agent = Pipeline()
-agent.add_component("llm", OpenAIChatGenerator(tools=tools))
-agent.add_component("tool_invoker", ToolInvoker(tools=tools)) 
-agent.add_component("router", MessageRouter())
-# agent.add_component(
-#     "adapter",
-#     OutputAdapter(
-#         template="{{ tool_messages }}",
-#         output_type=list[ChatMessage],
-#         unsafe=True,
-#     ),
-# )
-
-
-agent.connect("router.forAnalysis", "llm.messages")
-agent.connect("llm.replies", "router.messages")
-agent.connect("router.nextAction", "tool_invoker.messages")
-agent.connect("tool_invoker.tool_messages", "router.tool_messages")
-
-agent.draw("logo/agent.png") # Check on the design
+# Thats why I had to create SafeMCPTool to prevent errores due to deepcopying.
+agent = Agent(
+    chat_generator=OpenAIChatGenerator(tools=tools),
+    system_prompt="""
+    You are a helpful Agent that can read Github repositories content 
+    and create issues.
+    """,
+    tools=tools,
+)
 
 
 print("Agent created")
@@ -100,18 +67,19 @@ print("Agent created")
 user_input = f"""
 Fetch for the content of the README.md of the Github repository {github_repo_name}, if not available.
 If the content is available look for typos. If typos are found then create an issue in the repository.
-After the issue is created, return the number of typos found (0 if none).
+Return the number of typos found (0 if none), in json format
+<exemple>
+{
+    "typos_count": 1
+}
+</exemple>
 """
 
-response = agent.run({
-    "router": {
-        "from_user": [ChatMessage.from_user(user_input)]
-    }
-})
+agent.warm_up()
+response = agent.run(messages=[ChatMessage.from_user(user_input)])
 
 ## Print the agent thinking process
-print(response)
+# print(response)
 
-# Print the final response
-# for msg in response["response"]:
-#     print(msg.text)
+## Print the final response
+print(response["messages"][-1].text)
